144cadd808afcca3a939fa64d14b9cc0ad62fdd5
zhanghaocai <435504339@qq.com>
2024-08-06 04:31:29 +0800

    init project

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1624,
+    height: 750,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools()  // 允许F12打开console
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ renderer.js
@@ -0,0 +1,9 @@
+const information = document.getElementById('info')
+information.innerText = `本应用正在使用 Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), 和 Electron (v${versions.electron()})`
+
+const func = async () => {
+  const response = await window.versions.ping()
+  console.log(response)
+}
+
+func()
